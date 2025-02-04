dd12b9f8231a528886b125cd21864e30293e3765
joseantonionunes <joseantonionunes2018@gmail.com>
2025-02-04 16:24:00 -0300

    Apresentação de cada cliente

--- app/views/agent_clients.php
+++ app/views/agent_clients.php
@@ -16,48 +16,52 @@
 
             <hr>
 
-            <p class="my-5 text-center opacity-75">Não existem clientes registados.</p>
+            <?php if (empty($clients)): ?>
+                <p class="my-5 text-center opacity-75">Não existem clientes registados.</p>
+            <?php else: ?>
 
-            <table class="table table-striped table-bordered">
-                <thead class="table-dark">
-                    <tr>
-                        <th>Nome</th>
-                        <th class="text-center">Sexo</th>
-                        <th class="text-center">Data nascimento</th>
-                        <th>Email</th>
-                        <th class="text-center">Telefone</th>
-                        <th>Interesses</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>[Nome do cliente]</td>
-                        <td class="text-center">[Sexo]</td>
-                        <td class="text-center">[Data nascimento]</td>
-                        <td>[Email]</td>
-                        <td class="text-center">[Telefone]</td>
-                        <td>[Interesses]</td>
-                        <td class="text-end">
-                            <a href="#"><i class="fa-regular fa-pen-to-square me-2"></i>Editar</a>
-                            <span class="mx-2 opacity-50">|</span>
-                            <a href="#"><i class="fa-solid fa-trash-can me-2"></i>Eliminar</a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+                <table class="table table-striped table-bordered">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>Nome</th>
+                            <th class="text-center">Sexo</th>
+                            <th class="text-center">Data nascimento</th>
+                            <th>Email</th>
+                            <th class="text-center">Telefone</th>
+                            <th>Interesses</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>[Nome do cliente]</td>
+                            <td class="text-center">[Sexo]</td>
+                            <td class="text-center">[Data nascimento]</td>
+                            <td>[Email]</td>
+                            <td class="text-center">[Telefone]</td>
+                            <td>[Interesses]</td>
+                            <td class="text-end">
+                                <a href="#"><i class="fa-regular fa-pen-to-square me-2"></i>Editar</a>
+                                <span class="mx-2 opacity-50">|</span>
+                                <a href="#"><i class="fa-solid fa-trash-can me-2"></i>Eliminar</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
 
-            <div class="row">
-                <div class="col">
-                    <p class="mb-5">Total: <strong>[0]</strong></p>
+                <div class="row">
+                    <div class="col">
+                        <p class="mb-5">Total: <strong>[0]</strong></p>
+                    </div>
+                    <div class="col text-end">
+                        <a href="#" class="btn btn-secondary">
+                            <i class="fa-regular fa-file-excel me-2"></i>Exportar para XLSX
+                        </a>
+                    </div>
                 </div>
-                <div class="col text-end">
-                    <a href="#" class="btn btn-secondary">
-                        <i class="fa-regular fa-file-excel me-2"></i>Exportar para XLSX
-                    </a>
-                </div>
-            </div>
-            
+
+            <?php endif; ?>
+
         </div>
     </div>
 </div>
