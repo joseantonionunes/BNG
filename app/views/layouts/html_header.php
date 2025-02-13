205fd60b3771cdce946932ddb7349207cb0a57f3
joseantonionunes <joseantonionunes2018@gmail.com>
2025-02-13 09:52:57 -0300

    Adicionando Datatable.net

--- app/views/layouts/html_header.php
+++ app/views/layouts/html_header.php
@@ -22,5 +22,12 @@
         <link rel="stylesheet" href="assets/flatpickr/flatpickr.min.css">
         <script src="assets/flatpickr/flarpickr.js"></script>
     <?php endif; ?>
+
+    <!-- jquery -->
+     <script src="assets/jquery/jquery-3.6.0.min.js"></script>
+
+     <-- datatables -->
+     <link rel="stylesheet" href="assets/datatables/datatables.min.css">
+     <script src="assets/datatables/datatables.min.js"></script>
 </head>
 <body>
